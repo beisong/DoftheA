b316ce27b168b4df9913557e198911b3a1d07041
beisong <weisongyap@gmail.com>
2021-10-12 10:09:57 +0800

    Modify icons

--- client/fantasyhome.js
+++ client/fantasyhome.js
@@ -27,7 +27,7 @@ Template.fantasyhome.helpers({
     league_support_mvp: function () {
         return ReactiveMethod.call('getLeagueMVP', 'Support', false, false, false);
     },
-    league_offlane_mvp: function () {c
+    league_offlane_mvp: function () {
         return ReactiveMethod.call('getLeagueMVP', 'Offlane', false, false, false);
     },
 
---
+++ slocal_process.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+meteor run --port 3000 --settings settings.json
+
